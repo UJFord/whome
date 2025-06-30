930f6fa9157728fd9d32b2bb9886b9d9789216d8
John Ford Ulbata <ford.ulbata@d-connect.net>
2025-06-30 21:08:39 +0800

    after work notes

--- index.js
+++ index.js
@@ -15,38 +15,72 @@ function focusInput(el) {
     sel.addRange(range)
 }
 
-// add input after submit
 window.addEventListener("keydown", function (event) {
-
-    let marker = "<span class='input-caret'>></span>"
-    let inputTxt = "<span class='input-txt'></span>"
 
     if (event.key === "Enter") {
 
         event.preventDefault()
 
-        let inputElement = document.querySelector(".input-cmd.input-active")
-        let inputCmd = inputElement.textContent.toString()
-        inputElement.classList.remove("input-active")
-        inputElement.removeAttribute("contenteditable")
+        let active = this.document.querySelector(".input-active")
+        active.classList.remove("input-active", "active-border")
+        active.removeAttribute("contenteditable")
+        active.removeAttribute("spellcheck")
 
-        let index = inputCmd.indexOf(marker)
-        if (index !== -1) {
-            inputCmd = inputCmd.substring(index + marker.length)
-        }
+        console.log(active.textContent.toString())
 
-        let newElement = document.createElement("div")
-        newElement.classList.add("input-cmd", "input-active", "active-border")
-        newElement.setAttribute("contenteditable", "true")
-        newElement.setAttribute("spellcheck", "false")
-        newElement.innerHTML = marker
+        let newCmd = this.document.createElement("div")
+        newCmd.classList.add("input-cmd", "input-active", "active-border")
+        newCmd.setAttribute("contenteditable", "true")
+        newCmd.setAttribute("spellcheck", "false")
+        
+        let caret = this.document.createElement("span")
+        caret.classList.add("caret")
+        caret.textContent = ">"
 
-        let termContainer = document.querySelector(".term-container")
-        termContainer.appendChild(newElement)
+        newCmd.appendChild(caret)
+        let textNode = this.document.createTextNode("")
+        newCmd.appendChild(textNode)
 
+        let terminal = this.document.querySelector(".terminal")
+        terminal.appendChild(newCmd)
 
-        console.log(inputCmd)
-        focusInput(inputTxt)
-        console.log(newElement)
+        focusInput(newCmd)
+
     }
 })
+
+// add input after submit
+// window.addEventListener("", function (event) {
+
+//     let marker = "<span class='input-caret'>></span>"
+//     let newCmd = "<span class='input-txt'></span>"
+
+//     if (event.key === "Enter") {
+
+//         event.preventDefault()
+
+//         let inputElement = document.querySelector(".input-cmd.input-active")
+//         let inputCmd = inputElement.textContent.toString()
+//         inputElement.classList.remove("input-active")
+//         inputElement.removeAttribute("contenteditable")
+
+//         let index = inputCmd.indexOf(marker)
+//         if (index !== -1) {
+//             inputCmd = inputCmd.substring(index + marker.length)
+//         }
+
+//         let newElement = document.createElement("div")
+//         newElement.classList.add("input-cmd", "input-active", "active-border")
+//         newElement.setAttribute("contenteditable", "true")
+//         newElement.setAttribute("spellcheck", "false")
+//         newElement.innerHTML = marker
+
+//         let termContainer = document.querySelector(".term-container")
+//         termContainer.appendChild(newElement)
+
+
+//         console.log(inputCmd)
+//         focusInput(newCmd)
+//         console.log(newElement)
+//     }
+// })
